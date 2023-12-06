420da4775e73a7f2488e972cd87c9d0ae4aa5659
XXTY1 <126321434+XXTY1@users.noreply.github.com>
2023-12-06 22:06:01 +0530

    none

---
+++ NoodleEngine/Home/admin_data.py
@@ -0,0 +1,10 @@
+SEARCH_KEY = "AIzaSyAJgvb4ENzP9OAfj5cB78plJ0P_5AXH-Ks"
+SEARCH_ID = "841570ea48b1a41b7"
+COUNTRY = 'IN'
+
+SEARCH_URL="https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+RESULT_COUNT = 10
+
+
+
+# print(SEARCH_URL.format(key=SEARCH_KEY,cx=SEARCH_ID,query="Youtube",start=0))
